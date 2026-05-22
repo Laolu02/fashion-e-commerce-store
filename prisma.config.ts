411a0c5d90439296ee3fb491d68d72fc78c889ac
etinmax <maxwelletinosa@gmail.com>
2026-05-22 07:10:59 +0100

    fix: simplify prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,5 @@
-// prisma.config.ts
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-    seed: 'tsx prisma/seed.ts', // This tells Prisma how to run your seed file
-  },
-  datasource: {
-    url: env('DATABASE_URL'), // Required for PostgreSQL connection in v7
-  },
 });
